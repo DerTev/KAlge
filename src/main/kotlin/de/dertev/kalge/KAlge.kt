6c468affe32ac69ab245c2e9c57ffc01011a81e2
DwarflinDeveloping <dwarflin@gmx.de>
2021-07-12 15:41:10 +0200

    fixed loop

--- src/main/kotlin/de/dertev/kalge/KAlge.kt
+++ src/main/kotlin/de/dertev/kalge/KAlge.kt
@@ -1,5 +1,18 @@
 package de.dertev.kalge
 
+fun query() {
+    println()
+    print("Enter your command: ")
+    when(readLine()) {
+        "alge" -> println(randomAlge())
+        "color" -> println(randomColor())
+        "megaalge" -> println(randomMegaAlge())
+        "exit" -> return
+        else -> println("Command not found! \nCommands: alge, megaalge, color, exit")
+    }
+    query()
+}
+
 fun main() {
     println("__  ___      ___       __        _______  _______")
     println("|  |/  /     /   \\     |  |      /  _____||   ____|")
@@ -7,15 +20,9 @@ fun main() {
     println("|    <     /  /_\\  \\   |  |     |  | |_ | |   __|")
     println("|  .  \\   /  _____  \\  |  `----.|  |__| | |  |____")
     println("|__|\\__\\ /__/     \\__\\ |_______| \\______| |_______|   By DerTev")
-    println("")
-    println("")
-    println("Commands: alge, megaalge, color")
-    when(readLine()) {
-        "alge" -> println(randomAlge())
-        "color" -> println(randomColor())
-        "megaalge" -> println(randomMegaAlge())
-        else -> println("Command not found! \nCommands: alge, megaalge, color")
-    }
+    println()
+    println("Commands: alge, megaalge, color, exit")
+    query()
 }
 
 fun randomAlge(): String { /* generates a random alge from 2 to 6 */
